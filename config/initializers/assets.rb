b5dd9a610c4110a60a02a11cbacac306b92f3d53
kross1947 <57928337+kross1947@users.noreply.github.com>
2020-10-16 10:13:14 +0800

    Fix run on window docker issue
    Updated assert.rb and docker-compose.yaml

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,3 +17,11 @@ Rails.application.config.assets.precompile += [
   "*.js",
   "jquery.qtip.css",
 ]
+
+Rails.application.config.assets.configure do |env|
+  env.cache = Sprockets::Cache::FileStore.new(
+    ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
+    Rails.application.config.assets.cache_limit,
+    env.logger
+  )
+end
