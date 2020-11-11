a9002c4f680312370cf07dcf95d2258a49f5013a
Ts02o2o <pavilion15mini4@gmail.com>
2020-11-11 12:08:22 +0800

    TS
    Upload & download file from FTP server

--- app/models/concerns/downloadable_file.rb
+++ app/models/concerns/downloadable_file.rb
@@ -5,16 +5,26 @@ module DownloadableFile
   extend ActiveSupport::Concern
 
   included do
-    has_attached_file :file, PaperclipSettings.config.merge(
-      validate_media_type: false,
-      s3_url_options: { response_content_disposition: "attachment" } # this is ignored when using local storage
-    )
+    # has_attached_file :file, PaperclipSettings.config.merge(
+    #   validate_media_type: false,
+    #   s3_url_options: { response_content_disposition: "attachment" } # this is ignored when using local storage
+    # )
+
+    has_attached_file :file,
+    storage: :sftp,
+    path: Rails.configuration.sftp_config['sftp_upload_path'],
+    sftp_options: {
+      host: Rails.configuration.sftp_config['sftp_host'],
+      user: Rails.configuration.sftp_config['sftp_user'],
+      password: Rails.configuration.sftp_config['sftp_password'],
+      port: Rails.configuration.sftp_config['sftp_port'],  
+    }
 
     # TODO: Limit attachment types for safe uploads
     do_not_validate_attachment_file_type :file
   end
 
-  def download_url
+  def download_url  
     file.expiring_url
   end
 
