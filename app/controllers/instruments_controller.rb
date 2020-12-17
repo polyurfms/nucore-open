8362b6cfe6e215bb92ac12d8b132db31f13a40d4
Ts02o2o <pavilion15mini4@gmail.com>
2020-12-17 09:18:32 +0800

    Fixed -  product allows user input note
    In normal case, if the product allows user input note when booking, the order screen should a note field for user to input

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -39,12 +39,14 @@ class InstrumentsController < ProductsCommonController
   def show
     instrument_for_cart = InstrumentForCart.new(@product)
     # TODO: Remove this instance variable-not used anywhere but tests
-    @add_to_cart = false
-    if(has_delegated && session[:is_selected_user] == true) 
-      @add_to_cart = true
-    else 
-      @add_to_cart = instrument_for_cart.purchasable_by?(acting_user, session_user)
-    end
+    @add_to_cart = instrument_for_cart.purchasable_by?(acting_user, session_user)
+    
+    # @add_to_cart = false
+    # if(has_delegated && session[:is_selected_user] == true) 
+    #   @add_to_cart = true
+    # else 
+    #   @add_to_cart = instrument_for_cart.purchasable_by?(acting_user, session_user)
+    # end
     
     if @add_to_cart
       redirect_to new_facility_instrument_single_reservation_path(current_facility, @product)
