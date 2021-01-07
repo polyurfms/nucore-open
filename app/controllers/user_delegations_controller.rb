bcbe72045faa204b37535c9815d22f02fdaa63dc
Ts02o2o <pavilion15mini4@gmail.com>
2021-01-07 16:05:52 +0800

    Multiple delegation
    Multiple delegation

--- app/controllers/user_delegations_controller.rb
+++ app/controllers/user_delegations_controller.rb
@@ -19,7 +19,12 @@ class UserDelegationsController < ApplicationController
       @user  = User.find_by(username: session_user[:username])
       unless @user.nil?
         # @delegate_list = User.joins("LEFT JOIN user_delegations ON user_delegations.delegator = users.id WHERE user_delegations.delegatee LIKE '#{session_user[:username]}' or users.id = #{session_user[:id]}")
-        @delegate_list = User.joins("LEFT JOIN user_delegations ON user_delegations.delegator = users.id WHERE user_delegations.deleted_at IS NULL AND (user_delegations.delegatee LIKE '#{session_user[:username]}' or users.id = #{session_user[:id]} ) ORDER BY FIELD(users.id, #{session_user[:id]}) DESC , users.username ")
+        # @delegate_list = User.joins("LEFT JOIN user_delegations ON user_delegations.delegator = users.id WHERE user_delegations.deleted_at IS NULL AND (user_delegations.delegatee LIKE '#{session_user[:username]}' or users.id = #{session_user[:id]} ) ORDER BY FIELD(users.id, #{session_user[:id]}) DESC , users.username ")
+        @delegate_list1 = User.where(id: session_user[:id])  
+        @delegate_list2 = User.joins("LEFT JOIN user_delegations ON user_delegations.delegator = users.id WHERE user_delegations.deleted_at IS NULL AND user_delegations.delegatee LIKE '#{session_user[:username]}' ORDER BY users.username ")
+        
+        
+        @delegate_list = @delegate_list1 + @delegate_list2
       end
 
       unless  @delegate_list.size > 1
@@ -47,10 +52,14 @@ class UserDelegationsController < ApplicationController
   def index
     @user_delegation = UserDelegation.new
     @user_id = session_user[:id]
-    @assigned_list = UserDelegation.find_by ("delegator = #{session_user[:id]} AND deleted_at IS NULL " )
-    @is_assigned = !@assigned_list.nil?
+    # @assigned_list = UserDelegation.find_by ("delegator = #{session_user[:id]} AND deleted_at IS NULL " )
+    @assigned_list ||= UserDelegation.where(delegator: session_user[:id], deleted_at: nil)
+    
+    @is_assigned = false
+    @is_assigned = true if @assigned_list.count > 0
 
     count = User.check_academic_user_and_payment_source(@user_id).count
+
     unless(count > 0)
       redirect_to '/'
     end
@@ -58,28 +67,40 @@ class UserDelegationsController < ApplicationController
   end
 
   def create
-    @delegate_info = user_delegation_params
-    @user_delegation = UserDelegation.create(@delegate_info)
- 
-    delegatee= User.find_by(username: @delegate_info["delegatee"])
+    @delegate_info = user_delegation_params    
+    delegatee = User.find_by(username: @delegate_info["delegatee"].strip)
     delegator = User.find(session_user[:id])
-
-    LogEvent.log(@user_delegation, :create, delegator)
-
-    unless (@user_delegation.delegator.nil?)
-      if @user_delegation.save
-        UserDelegationMailer.notify(delegatee: delegatee, delegator: delegator).deliver_later
-        flash[:notice] = text("#{@user_delegation.delegatee} delegated")
-      else 
-        flash[:notice] = text("#{@user_delegation.delegatee} could not be delegated")
-      end 
+    
+    if (delegatee.nil?)
+      flash[:error] = text("#{@delegate_info["delegatee"].strip} cannot be found")
+    else 
+      if (delegatee.username.eql?(delegator.username))
+        flash[:error] = text("#{delegatee.username} can not delegated")
+      else
+        @assigned_list ||= UserDelegation.where(delegatee: delegatee.username, delegator: delegator.id, deleted_at: nil).count
+  
+        if (@assigned_list > 0)
+          flash[:error] = text("#{delegatee.username} has been delegated")
+        else   
+          unless (delegator.nil?)        
+            @user_delegation = UserDelegation.create(@delegate_info)
+            if @user_delegation.save
+              LogEvent.log(@user_delegation, :create, delegator)
+              UserDelegationMailer.notify(delegatee: delegatee, delegator: delegator).deliver_later
+              flash[:notice] = text("#{@user_delegation.delegatee} delegated")
+            else 
+              flash[:error] = text("#{@user_delegation.delegatee} could not be delegated")
+            end 
+          end
+        end
+      end
     end
+
     redirect_to action: :index
   end
 
   def destroy
-
-    begin
+     begin
       user_delegation = UserDelegation.find_by delegator: session_user[:id], id: params[:id].to_i
       delegator = User.find(session_user[:id])
     rescue ActiveRecord::RecordNotFound
