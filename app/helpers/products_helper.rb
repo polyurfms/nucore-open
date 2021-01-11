4792ea02e427afa2cb97dc1cb4e410157bf5ffd5
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-01-11 10:21:30 +0800

    update icon (#37)
    calendar icon bug fix

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -33,7 +33,7 @@ module ProductsHelper
     opts = if product.facility.show_instrument_availability? || !session_user.nil?
       public_calendar_availability_options(product)
     else
-      { class: ["fa fa-calendar fa-lg fa-fw"], title: t("instruments.public_schedule.icon") }
+      #{ class: ["fa fa-calendar fa-lg fa-fw"], title: t("instruments.public_schedule.icon") }
     end
 
     link_to (image_tag("calendar.png")), facility_instrument_public_schedule_path(product.facility, product), opts
