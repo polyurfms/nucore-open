eaa34b9432fd02521dda2aab1157fd6a09e50ade
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-02-03 14:56:47 +0800

    Update 20210114083930_create_fo_journals.rb

--- db/migrate/20210114083930_create_fo_journals.rb
+++ db/migrate/20210114083930_create_fo_journals.rb
@@ -3,6 +3,7 @@ class CreateFoJournals < ActiveRecord::Migration[5.2]
       create_table "fo_journals", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
         t.integer :journal_id
 			  t.string :status , limit: 25, null: true
+        t.string :type, limit:1, null:true
         t.timestamps null: false
       end
     end
