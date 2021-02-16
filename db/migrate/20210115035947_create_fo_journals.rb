e0f2aab7689fd3ee0aacd76b132e7edb0b8d682e
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-02-17 01:01:46 +0800

    remove duplidate

--- db/migrate/20210115035947_create_fo_journals.rb
+++
@@ -1,9 +0,0 @@
-class CreateFoJournals < ActiveRecord::Migration[5.2]
-	def change
-		create_table :"fo_journals" , id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-			t.integer "journal_id", null: true
-			t.string "jounral_type", limit:1, null:false
-			t.string "status", limit: 25, null: false
-		end
-	end
-end
