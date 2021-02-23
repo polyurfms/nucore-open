daab2c81db6c3240ea71ff27a47354324973225b
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-02-23 11:46:30 +0800

    UI update 20200219 (#69)
    - disable auto logout
    - add reservation breadcrumb
    - update label and format code
    - Add confirm end reservation dialog

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -5,6 +5,10 @@ module BreadcrumbHelper
   def order_reservation_breadcrumb
     link_to my_breadcrumb_label, my_breadcrumb_path
   end
+  
+  def order_detail_breadcrumb
+    link_to order_detail_breadcrumb_label, order_order_detail_path(@order_detail.order, @order_detail.id)
+  end
 
   private
 
@@ -20,6 +24,15 @@ module BreadcrumbHelper
   end
 
   def my_breadcrumb_path
+    public_send("#{@active_tab}_path")
+  end
+
+
+  def order_detail_breadcrumb_label
+    "Ref. No." + @order_detail.order_number
+  end
+
+  def order_detail_path
     public_send("#{@active_tab}_path")
   end
 
