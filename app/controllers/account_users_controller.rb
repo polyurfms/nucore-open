32414aecd9f3d61fae74a3509da7256f7ed9e2a4
Ts02o2o <pavilion15mini4@gmail.com>
2021-03-08 10:23:14 +0800

    Payment source add not in user table member
    Background:
    
    There are 2 ways to create URFMS user account. 1st is by admin manual create. 2nd is auto create by first login through SAML.
    Issue: User cannot assign payment source to student/staff if he/she does not already have URFMS account.
    
    Task: Implement a function to auto create URFMS account when assign payment source user

--- app/controllers/account_users_controller.rb
+++ app/controllers/account_users_controller.rb
@@ -8,7 +8,7 @@ class AccountUsersController < ApplicationController
   before_action :init_account
 
   load_and_authorize_resource
-
+  
   def initialize
     @active_tab = "accounts"
     super
@@ -16,6 +16,69 @@ class AccountUsersController < ApplicationController
 
   # GET /accounts/:account_id/account_users/user_search
   def user_search
+  end
+
+  def create_user
+    # account_id = params[:account_id];
+    # @facility = Facility.joins("INNER JOIN account_facility_joins on account_facility_joins.facility_id = facilities.id INNER JOIN accounts ON account_facility_joins.account_id = accounts.id WHERE accounts.id = #{account_id}")
+  end
+
+  # POST /facilities/:facility_id/users
+  def insert_user
+    @user = username_lookup(params[:username])
+    # authorize! :create, @user
+    if @user.nil?
+      flash[:error] = text("netid_not_found")
+      redirect_to account_account_users_path
+    elsif @user.persisted?
+      flash[:error] = text("user_already_exists", username: @user.username)
+      redirect_to account_account_users_path
+    elsif @user.save
+      LogEvent.log(@user, :create, current_user)
+      @user.create_default_price_group!
+      save_user_success(@user)
+
+      # Add Payment Source
+      @account_user = AccountUser.grant(@user, 'Purchaser', @account, by: session_user)
+      if @account_user.persisted?
+        LogEvent.log(@account_user, :create, current_user)
+        flash[:notice] = text("create.success", user: @user.full_name, account_type: @account.type_string)
+        redirect_to account_account_users_path(@account)
+      else
+        flash.now[:error] = text("create.error", user: @user.full_name, account_type: @account.type_string)
+        render :new
+      end
+    else
+      flash[:error] = text("create.error", message: @user.errors.full_messages.to_sentence)
+      redirect_to account_account_users_path
+    end
+  end
+
+  def save_user_success(user)
+    flash[:notice] = text("create.success")
+    if session_user.manager_of?(current_facility)
+      add_role = html("create.add_role", link: facility_facility_user_map_user_path(current_facility, user), inline: true)
+      flash[:notice].safe_concat(add_role)
+    end
+    # Notifier.new_user(user: user, password: user.password).deliver_later
+  end
+  
+  def add_user
+    @user = username_lookup(params[:username_lookup])
+    render layout: false
+  end
+
+  def service_username_lookup(username)
+    LdapAuthentication::UserLookup.new.call(username)
+  end
+
+  def username_lookup(username)
+    return nil if username.blank?
+    username_database_lookup(username.strip) || service_username_lookup(username.strip)
+  end
+
+  def username_database_lookup(username)
+    User.find_by("LOWER(username) = ?", username.downcase)
   end
 
   # GET /accounts/:account_id/account_users/new
