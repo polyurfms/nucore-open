2a2e2b0f47b5aa8922ccdec1c229a564d66141c1
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-03-09 09:29:49 +0800

    update maximum cost per day validation (#91)
    allow maximum cost/day = minimum cost

--- app/models/price_policy.rb
+++ app/models/price_policy.rb
@@ -208,7 +208,7 @@ class PricePolicy < ApplicationRecord
   def maximum_cost_validation
     unless (minimum_cost.nil?)
       if (!maximum_cost.nil? && maximum_cost > 0)
-          errors.add get_maximum_cost, ' must be greater than or equal to minimum cost' unless ((minimum_cost < maximum_cost)) 
+          errors.add get_maximum_cost, ' must be greater than or equal to minimum cost' unless ((minimum_cost <= maximum_cost)) 
       end
     end
     # return unless defined?(get_maximum_cost)
