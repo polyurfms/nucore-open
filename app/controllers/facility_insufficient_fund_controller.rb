ff551641e4bd1969468b64edb64bdf3a7bdb64e0
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-03-10 10:24:39 +0800

    Display all orders with insufficient fund for admin to review(#95)
    * link account to account free balance view
    
    * new scope to load insufficient fund orders

--- app/controllers/facility_insufficient_fund_controller.rb
+++ app/controllers/facility_insufficient_fund_controller.rb
@@ -3,7 +3,7 @@
 class FacilityInsufficientFundController < ApplicationController
 
   include SortableColumnController
-  
+
   admin_tab     :all
   before_action :authenticate_user!
   before_action :check_acting_as
@@ -24,23 +24,23 @@ class FacilityInsufficientFundController < ApplicationController
     raise ActionController::RoutingError.new("Notifications disabled with a zero-day review period") unless SettingsHelper.has_review_period?
   end
 
-  # GET /facilities/notifications
+  # GET /facilities/insufficient_fund
   def index
-    order_details = OrderDetail.need_notification.for_facility(current_facility)
+    order_details = OrderDetail.with_insufficient_fund.for_facility(current_facility)
     @search_form = TransactionSearch::SearchForm.new(params[:search])
     @search = TransactionSearch::Searcher.billing_search(order_details, @search_form, include_facilities: current_facility.cross_facility?)
     @date_range_field = @search_form.date_params[:field]
 
     if params[:sort].nil?
       @order_details = @search.order_details
-    else 
+    else
       @order_details = @search.order_details.reorder(sort_clause)
     end
-    
 
-    @order_details = @order_details.select{ |x| !x.has_sufficient_fund? }
-    
-    @order_detail_action = :send_notifications
+
+#    @order_details = @order_details.select{ |x| !x.has_sufficient_fund? }
+
+#    @order_detail_action = :send_notifications
   end
 
   # POST /facilities/notifications/send
@@ -116,13 +116,13 @@ class FacilityInsufficientFundController < ApplicationController
   end
 
   def sort_lookup_hash
-    {      
+    {
       "order_number" => "order_details.order_id",
       "fulfilled_date" => "order_details.fulfilled_at",
       "product_name" => "products.name",
       "ordered_for" => ["#{User.table_name}.last_name", "#{User.table_name}.first_name"],
       "payment_source" => "accounts.description",
-      "actual_subsidy" => "order_details.actual_cost", 
+      "actual_subsidy" => "order_details.actual_cost",
       # "actual_subsidy" => "order_details.actual_subsidy",
       "state" => "order_details.state",
     }
