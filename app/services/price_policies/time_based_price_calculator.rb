55af80cdeaf08760f02d0d771d688316c33563b5
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-05-07 15:28:01 +0800

    Update multiple day reservation estimated cost calculation (#154)

--- app/services/price_policies/time_based_price_calculator.rb
+++ app/services/price_policies/time_based_price_calculator.rb
@@ -42,6 +42,7 @@ module PricePolicies
       else
         costs.merge(subsidy: duration_mins * usage_subsidy * discount_multiplier)
       end
+
     end
 
     # Contain maximum cost
@@ -49,7 +50,9 @@ module PricePolicies
       result = 0.0
 
       count_day = Date.parse(end_at.strftime("%Y-%m-%d")) - Date.parse(start_at.strftime("%Y-%m-%d"))
-      discount_multiplier = calculate_discount(start_at, end_at)
+      # discount_multiplier = calculate_discount(start_at, end_at)
+      total_duration_mins = 0
+
       case 
       when count_day == 0
         # one day
@@ -60,25 +63,33 @@ module PricePolicies
         # Add "+ 1" for 23:59:59(end_of_day) + 1 secorc 
         # First date
         duration_mins = TimeRange.new(start_at, start_at.end_of_day).duration_mins
+        total_duration_mins = duration_mins + 1
         # result = over_maximum_cost(duration_mins, usage_rate, 0)
         result = over_maximum_cost(duration_mins + 1, usage_rate, 0)
+
         # Last date
         # duration_mins = TimeRange.new(end_at, end_at.end_of_day).duration_mins
         duration_mins = TimeRange.new(end_at.beginning_of_day, end_at).duration_mins
-        result = result + over_maximum_cost(duration_mins, usage_rate, 0)
+        total_duration_mins = total_duration_mins + duration_mins if end_at.beginning_of_day != end_at
+        result = result + over_maximum_cost(duration_mins, usage_rate, 0) if end_at.beginning_of_day != end_at
       else
         # more than two day
         # Add "+ 1" for 23:59:59(end_of_day) + 1 secorc 
         # First date
         duration_mins = TimeRange.new(start_at, start_at.end_of_day).duration_mins
+
+        total_duration_mins = duration_mins + 1
         # result = over_maximum_cost(duration_mins, usage_rate, 0)
         result = over_maximum_cost(duration_mins + 1, usage_rate, 0)
+
         # Last date
         duration_mins = TimeRange.new(end_at.beginning_of_day, end_at).duration_mins
+        total_duration_mins = total_duration_mins + duration_mins if end_at.beginning_of_day != end_at
         # duration_mins = TimeRange.new(end_at, end_at.end_of_day).duration_mins
-        result = result + over_maximum_cost(duration_mins, usage_rate, 0)
+        result = result + over_maximum_cost(duration_mins, usage_rate, 0) if end_at.beginning_of_day != end_at
         # Other date (60mins *24hr) = 1440
-        result = result + over_maximum_cost(1440, usage_rate, count_day - 1)
+        result = result + over_maximum_cost(1440, usage_rate, count_day - 1) 
+        total_duration_mins = total_duration_mins + (1440 * (count_day - 1))
       end
 
       costs = { cost: result * discount_multiplier }
@@ -86,7 +97,8 @@ module PricePolicies
       if costs[:cost] < minimum_cost.to_f
         { cost: minimum_cost, subsidy: minimum_cost_subsidy }
       else
-        costs.merge(subsidy: duration_mins * usage_subsidy * discount_multiplier)
+        # costs.merge(subsidy: duration_mins * usage_subsidy * discount_multiplier)
+        costs.merge(subsidy: total_duration_mins * usage_subsidy * discount_multiplier)
       end
 
     end
