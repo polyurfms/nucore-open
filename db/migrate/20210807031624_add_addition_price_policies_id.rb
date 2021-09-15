3bf278f113a65e71a2c2794673f988a4a7bf7b72
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-09-15 15:50:55 +0800

    rename addtion_price_policies to additional_price_policies

--- db/migrate/20210807031624_add_addition_price_policies_id.rb
+++
@@ -1,8 +0,0 @@
-class AddAdditionPricePoliciesId < ActiveRecord::Migration[4.2]
-  def change
-    
-    add_column :order_details, :addition_price_policy_type, :string, null: true
-    # add_column :order_details, :addition_price_policies_id, :integer, null: true
-    # execute "ALTER TABLE order_details ADD CONSTRAINT addition_price_policies_FK FOREIGN KEY (addition_price_policies_id) REFERENCES addition_price_policies(id)"
-  end
-end
