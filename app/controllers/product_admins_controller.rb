b864a02e0b4933ba05e77906d7aa1b03aada1056
polyurfms <80323961+polyurfms@users.noreply.github.com>
2021-10-29 11:37:53 +0800

    Update product_admins_controller.rb

--- app/controllers/product_admins_controller.rb
+++ app/controllers/product_admins_controller.rb
@@ -7,8 +7,6 @@ class ProductAdminsController < ApplicationController
 
   admin_tab :index, :new
   before_action :init_current_facility
-#  before_action :init_facility_staff
-
 
   load_and_authorize_resource
 
@@ -26,7 +24,6 @@ class ProductAdminsController < ApplicationController
   def index
     @facility_staffs = User.find_users_by_facility(current_facility)
     @product_admin_by_user = @product.product_admins.pluck(:user_id, :product_id).to_h
-    puts "<<< #{@product_admin_by_user}"
   end
 
   # POST /facilities/:facility_id/instruments/:instrument_id/update_restrictions
@@ -64,11 +61,6 @@ class ProductAdminsController < ApplicationController
 
   def downcase_product_type
     @product.class.model_name.human.downcase
-  end
-
-  def init_facility_staff
-    puts "init facility staff"
-    #@facility_staffs = User.find_users_by_facility(current_facility).build # for CanCan auth
   end
 
 end
