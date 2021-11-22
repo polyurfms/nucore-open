2364d9aa871af463e3e584d79b603982aa7317e3
SO <ksh1so@polyu.edu.hk>
2021-11-22 16:31:31 +0800

    Change relay display control
    Change relay display control

--- app/support/reservation_instrument_switcher.rb
+++ app/support/reservation_instrument_switcher.rb
@@ -9,46 +9,79 @@ class ReservationInstrumentSwitcher
   end
 
   def switch_on!
-    raise relay_error_msg unless reservation.can_switch_instrument_on?
-    if switch_relay_on
+    if relays_enabled?
+      ActiveRecord::Base.transaction do
+        begin
+          if @reservation.start_reservation!
+            instrument.instrument_statuses.create(is_on: true)
+            raise(ActiveRecord::Rollback) unless switch_relay_on
+          end
+        end
+      rescue => e
+        raise e
+        raise(ActiveRecord::Rollback)
+      end
+    else 
       @reservation.start_reservation!
-    else
-      raise relay_error_msg
     end
-    instrument.instrument_statuses.create(is_on: true)
+    
+   
+    # raise relay_error_msg unless reservation.can_switch_instrument_on?
+    # if switch_relay_on
+    #   @reservation.start_reservation!
+    # else
+    #   raise relay_error_msg
+    # end
+    # instrument.instrument_statuses.create(is_on: true)
   end
 
   def switch_off!
-    raise relay_error_msg unless reservation.can_switch_instrument_off?
-
-    if switch_relay_off == false
-      @reservation.end_reservation!
+    if relays_enabled?
+      ActiveRecord::Base.transaction do
+        begin
+          if @reservation.end_reservation!
+            instrument.instrument_statuses.create(is_on: false)
+            raise(ActiveRecord::Rollback) if switch_relay_off
+          end
+        end
+      rescue => e
+        raise e
+        raise(ActiveRecord::Rollback)
+      end
     else
-      raise relay_error_msg
+      @reservation.end_reservation!
     end
-    instrument.instrument_statuses.create(is_on: false)
+    
+    # raise relay_error_msg unless reservation.can_switch_instrument_off?
+
+    # if switch_relay_off == false
+    #   @reservation.end_reservation!
+    # else
+    #   raise relay_error_msg
+    # end
+    # instrument.instrument_statuses.create(is_on: false)
   end
 
   private
 
   def switch_relay_off
-    if relays_enabled?
-      relay.call_relay_user_info(@reservation.order_detail.user.username, @reservation.reserve_start_at.strftime('%d %b %Y %-l:%M %p'), @reservation.reserve_end_at.strftime('%d %b %Y %-l:%M %p'))
-      relay.deactivate
-      relay.get_status
-    else
-      false
-    end
+    # if relays_enabled?
+    relay.call_relay_user_info(@reservation.order_detail.user.username, @reservation.reserve_start_at.strftime('%d %b %Y %-l:%M %p'), @reservation.reserve_end_at.strftime('%d %b %Y %-l:%M %p'))
+    relay.deactivate
+    return relay.get_status
+    # else
+    #   false
+    # end
   end
 
   def switch_relay_on
-    if relays_enabled?
-      relay.call_relay_user_info(@reservation.order_detail.user.username, @reservation.reserve_start_at.strftime('%d %b %Y %-l:%M %p'), @reservation.reserve_end_at.strftime('%d %b %Y %-l:%M %p'))
-      relay.activate
-      relay.get_status
-    else
-      true
-    end
+    # if relays_enabled?
+    relay.call_relay_user_info(@reservation.order_detail.user.username, @reservation.reserve_start_at.strftime('%d %b %Y %-l:%M %p'), @reservation.reserve_end_at.strftime('%d %b %Y %-l:%M %p'))
+    relay.activate
+    return relay.get_status
+    # else
+    #   true
+    # end
   end
 
   def relays_enabled?
