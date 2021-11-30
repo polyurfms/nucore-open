2329d2826544a822e0220377d58b912c9e2d0836
kross1947 <57928337+kross1947@users.noreply.github.com>
2021-11-30 16:20:39 +0800

    update supervisor endorsement email

--- app/mailers/requset_endorsement_mailer.rb
+++ app/mailers/requset_endorsement_mailer.rb
@@ -2,17 +2,24 @@
 
 class RequsetEndorsementMailer < ActionMailer::Base
 
-  
+
   default from: Settings.email.from, content_type: "multipart/alternative"
 
-  def notify(to, requester, request_endorsement, first_name, last_name)
+  def notify(to, requester, request_endorsement, first_name, last_name, expiry_date)
+    puts "xxxxx #{requester.inspect}"
     @to = to
     @requester = requester
-    @request_endorsement = request_endorsement 
+    @request_endorsement = request_endorsement
     @first_name = first_name || ""
     @last_name = last_name || ""
     @to_fullname = @first_name + " " + @last_name
-    @request_fullname = requester.first_name + " " + requester.last_name
+    @dept_abbrev = requester.dept_abbrev.nil? ? " " : requester.dept_abbrev
+    @request_fullname = requester.first_name + " " + requester.last_name + " [" + @dept_abbrev + "]"
+
+    @requester_netID = @requester.username
+    @requester_post_title = @requester.post_title.nil? ? " " + @requester.post_title
+    @expiry_date = expiry_date
+
     send_nucore_mail @to, text("views.request_endorsements.subject", requester_name: @request_fullname)
   end
 
