3a438e46b354afc4b78b498bc08099dec4f01814
Ts02o2o <73634594+Ts02o2o@users.noreply.github.com>
2021-12-15 11:50:28 +0800

    Change relay search query function
    Change relay search query function

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -129,8 +129,12 @@ class ApiController < ApplicationController
     is_on = false
     unless relayIp.blank? && !relayIp.eql?(request.ip)
       @product = Product.joins("INNER JOIN relays on relays.instrument_id  = products.id  WHERE relays.ip = '#{relayIp}'")
-      @relay = Relay.find_by(ip: relayIp)
+      @relay = Relay.where("ip = '#{relayIp}'")
       if @product.length > 0  && @relay.length > 0
+        puts "......product.........."
+        puts @product.length 
+        puts ".......relay........."
+        puts @relay.length 
         result = Hash.new
         # result["outlet"] = @relay.outlet
         # result["name"] = @product.first.name
